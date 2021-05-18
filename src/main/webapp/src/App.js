6ecfbc7f9f8dfe7c847355f056e373be83bbd2f8
Boris Jancic <jancicboris56@gmail.com>
2021-05-18 11:47:10 +0200

    New routes added

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from "./components/NavbarComponent";
-import RegisterLayout from "./layouts/RegisterLayout";
 import HomeLayout from "./layouts/HomeLayout";
 import LoginLayout from "./layouts/LoginLayout";
 import BrowseLayout from "./layouts/BrowseLayout";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import EditArticle from "./components/EditArticleComponent";
 import AddArticle from "./components/AddArticleComponent";
 import {PrivateRoute} from "./components/PrivateRoute";
 import NotFound from "./layouts/NotFound";
+import Register from "./components/RegisterComponent";
+import UserProfile from "./components/UserProfile";
+import {UserTable} from "./components/UserTable";
+import AdminLayout from "./layouts/AdminLayout";
 
 function App() {
   return (
@@ -18,7 +21,7 @@ function App() {
         <Router>
           <Switch>
               <Route path="/home" exact component={HomeLayout}/>
-              <Route path="/register" exact component={RegisterLayout}/>
+              <Route path="/register" exact component={Register}/>
               <Route path="/login" exact component={LoginLayout}/>
               <PrivateRoute
                   path="/addArticle"
@@ -26,11 +29,17 @@ function App() {
                   component={AddArticle}
                   roles={["ROLE_SELLER"]}/>
               <PrivateRoute
+                  path="/users"
+                  exact
+                  component={AdminLayout}
+                  roles={["ROLE_ADMIN"]}/>
+              <PrivateRoute
                   path="/editArticle/:id"
                   exact
                   component={EditArticle}
                   roles={["ROLE_SELLER"]}/>
               <Route path="/browse" exact component={BrowseLayout}/>
+              <Route path="/profile" exact component={UserProfile}/>
               <Route component={NotFound} />
           </Switch>
         </Router>
