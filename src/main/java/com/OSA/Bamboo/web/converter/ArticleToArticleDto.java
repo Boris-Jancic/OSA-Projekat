9fcfa08b10b77ca9f8b5ac793a5affd43308c706
Boris Jancic <jancicboris56@gmail.com>
2021-06-22 16:35:05 +0200

    The converter will now find the appropriate picture for the given article

--- src/main/java/com/OSA/Bamboo/web/converter/ArticleToArticleDto.java
+++ src/main/java/com/OSA/Bamboo/web/converter/ArticleToArticleDto.java
@@ -1,29 +1,49 @@
-package com.OSA.Bamboo.web.converter.converter;
+package com.OSA.Bamboo.web.converter;
 
 import com.OSA.Bamboo.dto.ArticleDto;
 import com.OSA.Bamboo.model.Article;
+import org.apache.commons.io.FileUtils;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Iterator;
 import java.util.List;
 
 @Component
 public class ArticleToArticleDto implements Converter<Article, ArticleDto> {
+    private final String imageDirectory = System.getProperty("user.dir") + "/images/";
     public ArticleToArticleDto() {
     }
 
     @Override
     public ArticleDto convert(Article article) {
         ArticleDto dto = new ArticleDto();
+
+        File imgPath = new File(imageDirectory);
+        byte[] fileContent = new byte[0];
+
+        try {
+            fileContent = FileUtils.readFileToByteArray(new File(imgPath + "/" + article.getImageName()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String encodedString = Base64.getEncoder().encodeToString(fileContent);
+
+        dto.setId(article.getId());
         dto.setName(article.getName());
         dto.setDescription(article.getDescription());
         dto.setPrice(article.getPrice());
+        dto.setImageBytes(encodedString);
+
         return dto;
     }
 
-    public List<ArticleDto> convert(List<Article> articles) {
+    public List<ArticleDto> convert(List<Article> articles) throws IOException {
 
         List<ArticleDto> retVal = new ArrayList();
         Iterator articleIterator = articles.iterator();
