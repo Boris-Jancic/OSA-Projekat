919bbb4c4f5735e5704b586867a145a837db1a93
Boris Jancic <jancicboris56@gmail.com>
2021-06-28 22:50:23 +0200

    Added id

--- src/main/java/com/OSA/Bamboo/dto/UserDto.java
+++ src/main/java/com/OSA/Bamboo/dto/UserDto.java
@@ -1,25 +1,34 @@
 package com.OSA.Bamboo.dto;
 
 import com.OSA.Bamboo.model.enums.UserRole;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public abstract class UserDto {
+public class UserDto {
 
-    @NotBlank(message = "Username is mandatory")
+    @NotNull
+    private Long id;
+    @NotBlank(message = "LastName is mandatory")
     private String username;
-
-    @NotBlank(message = "Name is mandatory")
+    @NotBlank(message = "LastName is mandatory")
+    private String password;
+    @NotBlank(message = "LastName is mandatory")
     private String name;
-
     @NotBlank(message = "LastName is mandatory")
     private String lastName;
 
+    @Enumerated(EnumType.STRING)
     private UserRole role;
 
     private boolean blocked;
